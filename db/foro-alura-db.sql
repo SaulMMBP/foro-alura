8a27d0e778498ec80d7fa9dd2a7fac1606ae8c44
Saul Malagon Martinez <sark_blues_friki95@hotmail.com>
2023-05-01 16:35:32 -0500

    Add database sql script

---
+++ db/foro-alura-db.sql
@@ -0,0 +1,15 @@
+DROP SCHEMA IF EXISTS foro_alura;
+CREATE DATABASE foro_alura;
+
+USE foro_alura;
+
+CREATE TABLE topicos (
+    id_topico BIGINT AUTO_INCREMENT NOT NULL,
+    titulo VARCHAR(100) NOT NULL,
+    mensaje TEXT NOT NULL,
+    fecha_creacion DATE NOT NULL,
+    estado TINYINT NOT NULL DEFAULT 0,
+    autor VARCHAR(200) NOT NULL,
+    curso VARCHAR(200) NOT NULL,
+    PRIMARY KEY (id_topico)
+);
