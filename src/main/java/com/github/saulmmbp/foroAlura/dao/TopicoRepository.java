f61166e5e6a78adcb5067b7ab8f6ffe4df747c2d
Saul Malagon Martinez <sark_blues_friki95@hotmail.com>
2023-05-02 14:32:34 -0500

    add topico repository

---
+++ src/main/java/com/github/saulmmbp/foroAlura/dao/TopicoRepository.java
@@ -0,0 +1,9 @@
+package com.github.saulmmbp.foroAlura.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.github.saulmmbp.foroAlura.entity.Topico;
+
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+}
