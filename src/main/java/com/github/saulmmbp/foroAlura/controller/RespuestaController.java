05ebb7f2d86f281c87fd4170647b7bb6ee52ae15
Saul Malagon Martinez <sark_blues_friki95@hotmail.com>
2023-05-04 14:34:39 -0500

    added endpoint to CRUD

--- src/main/java/com/github/saulmmbp/foroAlura/controller/RespuestaController.java
+++ src/main/java/com/github/saulmmbp/foroAlura/controller/RespuestaController.java
@@ -6,9 +6,10 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.hateoas.*;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import com.github.saulmmbp.foroAlura.dto.*;
+import com.github.saulmmbp.foroAlura.dto.RespuestaDto;
 import com.github.saulmmbp.foroAlura.service.RespuestaService;
 import com.github.saulmmbp.foroAlura.util.RespuestaModelAssembler;
 
@@ -36,5 +37,27 @@ public class RespuestaController {
 	public EntityModel<RespuestaDto> getRespuesta(@PathVariable Long id) {
 		RespuestaDto respuesta = respuestaService.findById(id);
 		return respuestaAssembler.toModel(respuesta);
+	}
+	
+	@PostMapping
+	public ResponseEntity<EntityModel<RespuestaDto>> newRespuesta(@RequestBody RespuestaDto respuestaDto) {
+		EntityModel<RespuestaDto> respuesta = respuestaAssembler.toModel(respuestaService.save(respuestaDto));
+		return ResponseEntity
+				.created(respuesta.getRequiredLink(IanaLinkRelations.SELF).toUri())
+				.body(respuesta);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<EntityModel<RespuestaDto>> updateRespuesta(@RequestBody RespuestaDto respuestaDto, @PathVariable Long id) {
+		EntityModel<RespuestaDto> respuesta = respuestaAssembler.toModel(respuestaService.update(respuestaDto, id));
+		return ResponseEntity
+				.created(respuesta.getRequiredLink(IanaLinkRelations.SELF).toUri())
+				.body(respuesta);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deleteRespuesta(@PathVariable Long id) {
+		respuestaService.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/github/saulmmbp/foroAlura/service/RespuestaService.java
+++ src/main/java/com/github/saulmmbp/foroAlura/service/RespuestaService.java
@@ -1,6 +1,7 @@
 package com.github.saulmmbp.foroAlura.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
@@ -8,7 +9,10 @@ import com.github.saulmmbp.foroAlura.dao.RespuestaRepository;
 import com.github.saulmmbp.foroAlura.dto.RespuestaDto;
 import com.github.saulmmbp.foroAlura.entity.Respuesta;
 
+import jakarta.transaction.Transactional;
+
 @Service
+@Transactional
 public class RespuestaService {
 
 	private RespuestaRepository respuestaRepository;
@@ -18,10 +22,27 @@ public class RespuestaService {
 	}
 	
 	public List<RespuestaDto> findAll() {
-		return respuestaRepository.findAll().stream().map(Respuesta::toDto).toList();
+		return respuestaRepository.findAll().stream()
+				.map(Respuesta::toDto)
+				.collect(Collectors.toList());
 	}
 	
 	public RespuestaDto findById(Long id) {
 		return respuestaRepository.findById(id).orElseThrow().toDto();
+	}
+	
+	public RespuestaDto save(RespuestaDto respuestaDto) {
+		return respuestaRepository.save(respuestaDto.toEntity()).toDto();
+	}
+	
+	public RespuestaDto update(RespuestaDto respuestaDto, Long id) {
+		Respuesta respuesta = respuestaRepository.findById(id).orElseThrow();
+		respuesta.setMensaje(respuestaDto.mensaje());
+		respuesta.setSolucion(respuestaDto.solucion());
+		return respuestaRepository.save(respuesta).toDto();
+	}
+	
+	public void delete(Long id) {
+		respuestaRepository.deleteById(id);
 	}
 }
